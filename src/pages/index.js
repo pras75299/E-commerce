8c78c0d604da37659b9fb0cc33bbd23dce9cf56d
pras75299 <singh.prashantking@gmail.com>
2022-03-03 00:20:39 +0530

    add all required import in index.js and export

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import PrivateRoute from "./PrivateRoute";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Error from "./ErrorPage";
+import Product from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+export {
+  Home,
+  About,
+  PrivateRoute,
+  Cart,
+  Checkout,
+  Product,
+  SingleProduct,
+  Error,
+};
